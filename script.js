55cbce9369448c8d649d904d948b92a87c39295f
Tina Stanczyk <cstanczyk123@gmail.com>
2021-12-11 18:45:06 -0500

    Added pseudo code into js file

---
+++ script.js
@@ -0,0 +1,23 @@
+// add a timer variable
+
+// add an event listener to listen for when the start button is pressed
+
+//if start button is pressed then switch to first question 
+
+//when start button is pressed, start the timer
+
+// add event listener to listen for a click on one of the answers
+
+// if the answer is wrong then subtract time from the timer and switch to next question
+
+// if the timer is on 0 then end the game and switch to saying score and prompting user to enter their initials 
+
+// if the answer is right then switch to the next question
+
+//if the user is on the last question, then switch to saying score and prompting user to enter their initials 
+
+// save the user's score and initials in local storage as an array of objects
+
+// objects will contain initials and score, the array will contain the multiple users that play
+
+//when the user clicks on 'view highscores' they are presented with a table of initials and scores next to them, ranked from highest to lowest.
