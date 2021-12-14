f32b19c53ee3c8e6253fa958bffc58db8eb8afb0
Tina Stanczyk <cstanczyk123@gmail.com>
2021-12-13 21:42:05 -0500

    Completed saving highscores and initials in localstorage and viewing them when 'view highscores' is clicked

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ D = document.getElementById("optD");
 answers = document.getElementById("answers");
 scoreScreen = document.getElementById("scoreScrn");
 userScore = document.getElementById("usrScore");
+userInitials = document.getElementById("user-init");
+submitButton = document.getElementById("submit");
+viewHS = document.getElementById("viewHS");
+highScreen = document.getElementById("highscoreScrn");
+userRankings = document.getElementById("scoreList");
 // Creating an array of objects to hold all of the questions with their respective options and correct answer
 var quizQuest = [
   {
@@ -59,40 +64,80 @@ var timeLeft = 60;
 var index = 0;
 var score = 0;
 var opt = 0;
+// This function holds my timer and is called at the start of the quiz
 function time() {
   var timeInterval = setInterval(function () {
+    // If the timer hasn't reached zero, subtract 1 from timeLeft every 1000 milliseconds
     if (timeLeft > 0) {
       timerFig.textContent = "Timer: " + timeLeft;
       timeLeft--;
     }
+    //if the user is on the last question, then switch to saying score and prompting user to enter their initials
     if (opt === 5) {
       getScore(timeLeft);
       console.log(timeLeft);
       timerFig.textContent = "Timer: " + timeLeft;
-      clearInterval(timeInterval); 
+      clearInterval(timeInterval);
     }
+    // if the timer is on 0 then end the game and switch to saying score and prompting user to enter their initials
     if (timeLeft === 0 || timeLeft < 0) {
       getScore(timeLeft);
       console.log(timeLeft);
       timerFig.textContent = "Timer: " + timeLeft;
       clearInterval(timeInterval);
-    } 
-    else {
+    } else {
       timerFig.textContent = "Timer: " + timeLeft;
       //switch to screen that says score
     }
   }, 1000);
 }
 
-function getScore(){
+function getScore() {
   score = timeLeft;
   questionScreen.style.display = "none";
   question.dataset.state = "hidden";
   scoreScreen.style.display = "block";
   scoreScreen.dataset.state = "shown";
   userScore.textContent = "Score: " + score;
-
+  // save the user's score and initials in local storage as an array of objects
+  submitButton.addEventListener("click", function saveScore(event) {
+    event.preventDefault();
+    console.log(score);
+    console.log(userInitials.value);
+    // objects will contain initials and score, the array will contain the multiple users that play
+    var userScores = JSON.parse(localStorage.getItem("userScores") || "[]");
+    var user = {
+      usr: userInitials.value.trim(),
+      hscore: timeLeft,
+    };
+    userScores.push(user);
+    localStorage.setItem("userScores", JSON.stringify(userScores));
+    console.log(userScores);
+  });
 }
+//when the user clicks on 'view highscores' they are presented with a table of initials and scores next to them, ranked from highest to lowest.
+viewHS.addEventListener("click", function viewHighScores() {
+  questionScreen.style.display = "none";
+  questionScreen.dataset.state = "hidden";
+  startScreen.style.display = "none";
+  startScreen.dataset.state = "hidden";
+  scoreScreen.style.display = "none";
+  scoreScreen.dataset.state = "hidden";
+  timerFig.style.display = "none";
+  timerFig.dataset.state = "hidden";
+  highScreen.style.display = "block";
+  highScreen.dataset.state = "shown";
+  var hsList = JSON.parse(localStorage.getItem("userScores"));
+  for(let i=0; i<hsList.length; i++){
+    var userI = hsList[i].usr;
+    var uHS = hsList[i].hscore;
+    console.log(hsList[i].usr);
+    console.log(hsList[i].hscore);
+    var li = document.createElement("li");
+    li.textContent = "User: " + userI + " Score: " + uHS;
+    userRankings.append(li);
+  }
+});
 
 // add an event listener to listen for when the start button is pressed
 startButton.addEventListener("click", function () {
@@ -142,24 +187,9 @@ optContainer.addEventListener("click", function (event) {
     if (index < 3) {
       index++;
     }
-
   }
 });
 
 
-
-
-
-// if the timer is on 0 then end the game and switch to saying score and prompting user to enter their initials
-
-// if the answer is right then switch to the next question
-
-//if the user is on the last question, then switch to saying score and prompting user to enter their initials
-
-// save the user's score and initials in local storage as an array of objects
-
-// objects will contain initials and score, the array will contain the multiple users that play
-
-//when the user clicks on 'view highscores' they are presented with a table of initials and scores next to them, ranked from highest to lowest.
 
 //when user clicks on 'view highscores' add an event listener to listen for a click on the button 'clear highscores' to clear all local storage or 'go back' to bring the user back to the start button screen
